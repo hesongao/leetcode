84993e39dc5eed09a9b49a330e957c4e17107233
hesongao <hesongao1993>
2024-03-31 18:53:34 +0800

    二分查找

---
+++ src/main/java/leetcode/BinarySearch.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+/**
+ * 二分查找
+ * 给定一个 n 个元素有序的（升序）整型数组 nums 和一个目标值 target  ，写一个函数搜索 nums 中的 target，如果目标值存在返回下标，否则返回 -1。
+ */
+public class BinarySearch {
+    public int search(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+
+        while(start <= end){
+            int mid = (end - start) / 2 + start;
+            if(target == nums[mid]){
+                return mid;
+            }else if(target > nums[mid]){
+                start = mid + 1;
+            }else{
+                end = mid -1;
+            }
+        }
+        return -1;
+    }
+}
